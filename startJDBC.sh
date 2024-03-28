6f1217d7750fa1edd6a57a927094102e9fd46222
IndyHendrickx <92083621+IndyHendrickx@users.noreply.github.com>
2024-03-28 18:59:05 +0100

    Update startJDBC.sh

--- startJDBC.sh
+++ startJDBC.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -cp '/opt/FileMaker/JDBC/java/lib/pjbridge.jar:/opt/FileMaker/JDBC/java/lib/commons-daemon-1.2.2.jar:/opt/FileMaker/JDBC/java/lib/fmjdbc.jar' Server com.filemaker.jdbc.Driver 4444 &
+java -cp '/opt/FileMaker/JDBC/java/lib/pjbridge.jar:/opt/FileMaker/JDBC/java/lib/commons-daemon-1.2.2.jar:/opt/FileMaker/JDBC/java/lib/fmjdbc.jar' Server com.filemaker.jdbc.Driver 4444
