879b4ae4850249842c14a8e94e43707a7e0f40f9
IndyHendrickx <92083621+IndyHendrickx@users.noreply.github.com>
2024-09-10 14:21:37 +0200

    Create test.php

---
+++ test.php
@@ -0,0 +1,29 @@
+<?php
+require "./PHP/PJBridge.php";
+
+// Default
+$host = 'localhost';
+$port = '4444';
+
+// Get username/password and database from cli
+$user = $argv[1];
+$password = $argv[2];
+$database = $argv[3];
+
+// Get SQL query to execute from CLI
+$sql = $argv[4];
+
+// Try to create a connection
+$bridge = new PJBridge($host, $port, 'utf-8', 'utf-8');
+$connectString = "jdbc:filemaker://$host/$database";
+$connection = $bridge->connect($connectString, $user, $password);
+if (!$connection) {
+    throw new Exception("JDBC Connection failed to resolve...");
+}
+
+// Try to receive data
+$cursor = $bridge->exec($sql);
+while($row = $bridge->fetch_array($cursor){
+      print_r($row);
+}
+$bridge->free_result($cursor);
